bb9fc4520750d10f367ba31134a7241f9801bec8
nmelnychenko <mykola.melnichenko@gmail.com>
2021-06-14 09:55:51 +0300

    added header submenu script into main

--- src/js/index.js
+++ src/js/index.js
@@ -36,3 +36,57 @@ if ($(".slider").length) {
     ]
   });
 }
+
+// Script for dropdown header
+const dropdownTrigger = $(".header-dropdown-control");
+const dropdownMenu = $(".header__nav");
+const submenuTrigger = $(".header-navigation__item--submenu");
+const firstSubmenu = $(".first-submenu");
+const firstMenuBackTrigger = $(".first-submenu__back-trigger");
+const secondSubmenuTrigger = $(".first-submenu-list__item--submenu");
+const secondSubmenu = $(".second-submenu");
+const secondSubmenuBackTrigger = $(".second-submenu__back-trigger");
+
+document.addEventListener("click", function(e) {
+  if (!dropdownMenu[0].contains(e.target) && !dropdownTrigger.contains(e.target)) {
+    dropdownTrigger.removeClass("is-open");
+    dropdownMenu.removeClass("is-open");
+    firstSubmenu.removeClass("is-open");
+  }
+})
+
+if (dropdownTrigger.length) {
+  dropdownTrigger.addEventListener("click", function() {
+    dropdownTrigger.toggleClass("is-open");
+    dropdownMenu.toggleClass("is-open");
+  });
+}
+
+if (submenuTrigger.length) {
+  submenuTrigger.addEventListener("click", function(e) {
+    if (!firstSubmenu.contains(e.target)) {
+      submenuTrigger.toggleClass("is-open");
+      firstSubmenu.toggleClass("is-open");
+    }
+  });
+}
+
+if (firstMenuBackTrigger.length) {
+  firstMenuBackTrigger.addEventListener("click", function(e) {
+    e.stopPropagation();
+    $(this).parent.removeClass("is-open");
+  });
+}
+
+if (secondSubmenuTrigger.length) {
+  secondSubmenuTrigger.addEventListener("click", function() {
+    $(this).find(secondSubmenu).addClass("is-open");
+  });
+}
+
+if (secondSubmenuBackTrigger.length) {
+  secondSubmenuBackTrigger.addEventListener("click", function(e) {
+    e.stopPropagation();
+    $(this).parent.removeClass("is-open");
+  });
+}
